f30e9d3fa9fa87df8a9654edd738ad95da5290a1
Jacob McSwain <jacob@mcswain.dev>
2024-07-03 23:33:59 -0500

    make demo token configurable

--- public/config.js
+++ public/config.js
@@ -2,3 +2,4 @@ window.COMMA_URL_ROOT = 'https://openpilot.jacob.network/';
 window.USERADMIN_URL_ROOT = 'https://useradmin.comma.ai/';
 window.GOOGLE_REDIRECT_LINK = 'https://accounts.google.com/o/oauth2/auth?type=web_server&client_id=841898570930-4avg0csiu3v3hgqp94eifh19pjigujvh.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fopenpilot.jacob.network%2Fv2%2Fauth%2Fg%2Fredirect%2F&response_type=code&scope=openid&prompt=select_account';
 window.GITHUB_REDIRECT_LINK = 'https://github.com/login/oauth/authorize?client_id=Ov23liF4KlrRqyHkvIVP&redirect_uri=https%3A%2F%2Fopenpilot.jacob.network%2Fv2%2Fauth%2Fh%2Fredirect%2F&scope=read%3Auser';
+window.DEMO_AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDg1ODI0NjUsIm5iZiI6MTcxNzA0NjQ2NSwiaWF0IjoxNzE3MDQ2NDY1LCJpZGVudGl0eSI6IjBkZWNkZGNmZGYyNDFhNjAifQ.g3khyJgOkNvZny6Vh579cuQj1HLLGSDeauZbfZri9jw'
--- src/components/anonymous.jsx
+++ src/components/anonymous.jsx
@@ -109,7 +109,7 @@ class AnonymousLanding extends Component {
     const { classes } = this.props;
 
     const loginAsDemoUser = function() {
-      AuthStorage.setCommaAccessToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDg1ODI0NjUsIm5iZiI6MTcxNzA0NjQ2NSwiaWF0IjoxNzE3MDQ2NDY1LCJpZGVudGl0eSI6IjBkZWNkZGNmZGYyNDFhNjAifQ.g3khyJgOkNvZny6Vh579cuQj1HLLGSDeauZbfZri9jw');
+      AuthStorage.setCommaAccessToken(window.DEMO_AUTH_TOKEN);
       window.location = window.location.origin;
     };
 
