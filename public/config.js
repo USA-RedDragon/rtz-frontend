3ce159ede987e68fb1743a322ed9772234245e3c
Jacob McSwain <jacob@mcswain.dev>
2024-07-12 02:21:23 -0500

    Use my own HERE API key

--- public/config.js
+++ public/config.js
@@ -7,7 +7,7 @@ window.ATHENA_URL_ROOT = 'https://openpilot.jacob.network/';
 window.BILLING_URL_ROOT = 'https://openpilot.jacob.network/';
 window.MAPBOX_STYLE = 'mapbox://styles/usa-reddragon/clyd2ske5010r01qr2lde0k3n';
 window.MAPBOX_TOKEN = 'pk.eyJ1IjoidXNhLXJlZGRyYWdvbiIsImEiOiJjbHlkMzl6cW4wMGtnMmxvcWY1MTZpeGg2In0.AHfA3k-lG_b48str7o3xQw'
-window.HERE_API_KEY = 'O0atgmTwzKnwYJL2hk5N5qqG2R9y78f5GdHlvr_mtiw';
+window.HERE_API_KEY = 'gQ6y5miQkOPdgEMnkoNJHz3JgR2eD5pbLSmgzkhNyBs';
 window.CUSTOM_REDIRECT_LINK = 'https://authentik.mcswain.dev/login/oauth/authorize?response_type=code&client_id=ZYml3VDAwur3Y75MLvYy5rXO5dUrUIR6UlKOMdGh&redirect_uri=https%3A%2F%2Fopenpilot.jacob.network%2Fv2%2Fauth%2Fc%2Fredirect%2F&scope=user%3aemail';
 window.CUSTOM_ENABLED = true;
 window.CUSTOM_NAME = 'Authentik';
