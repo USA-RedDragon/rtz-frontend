fc860576a16caf8612fc058edc870419a5390dde
Jacob McSwain <jacob@mcswain.dev>
2024-07-19 16:52:25 -0500

    fixup e2e tests

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,3 +2,7 @@ import 'cypress-mochawesome-reporter/register';
 import { registerCommand } from 'cypress-wait-for-stable-dom';
 
 registerCommand();
+
+Cypress.on("window:before:load", win => {
+  win.indexedDB.deleteDatabase("localforage");
+});
